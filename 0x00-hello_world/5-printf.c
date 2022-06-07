6f1dcfb8549559b14033b32973e2fa0eea410002
Creed <79368421+MutukuSimon@users.noreply.github.com>
2022-06-07 04:11:28 +0300

    5
    5 print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,9 @@
 /**
 * main - prints the text with proper grammar,
 * but the outcome is a piece of art,
+* int: entry
+* printf: prints text
+* return: returns value
 *
 * return: 0 always if success
 */
